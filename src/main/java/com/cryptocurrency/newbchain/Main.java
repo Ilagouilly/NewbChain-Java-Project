25a2ada484d5b0cab4bb9b7fe17bbb3151310e04
closeupapp <closeupapp@gmail.com>
2021-10-02 17:02:27 +0200

    feat: implement chain validation & add mining feature

--- src/main/java/com/cryptocurrency/newbchain/Main.java
+++ src/main/java/com/cryptocurrency/newbchain/Main.java
@@ -17,16 +17,54 @@ public class Main {
 
         // Display opening banner
         displayScoresImplSystemOut.displayOpeningBanner();
+        
+        Block block1 = new Block("Thomas sends 1n to Jean-françois", "0");
+        block1.mineBlock(prefix);
+        newbchain.add(block1);
 
-        newbchain.add(new Block("Thomas sends 1n to Jean-françois", "0"));		
-		newbchain.add(new Block("Jean-françois sends 0.1n to Taqueria el farolito", newbchain.get(newbchain.size()-1).getHash())); 
-		newbchain.add(new Block("José sends 5n to Maria", newbchain.get(newbchain.size()-1).getHash()));
+        Block block2 = new Block("Jean-françois sends 0.1n to Taqueria el farolito", newbchain.get(newbchain.size()-1).getHash());	
+		block2.mineBlock(prefix);
+        newbchain.add(block2);   
+
+		Block block3 = new Block("José sends 5n to Maria", newbchain.get(newbchain.size()-1).getHash()); 
+        block3.mineBlock(prefix);
+        newbchain.add(block3);
 		
         for(Block nb : newbchain){
             System.out.println(nb.toString() + "\n\n");
         }
+
+        System.out.println("Checking if the Chain is valid: "+isChainValid());
 		
         // Display closing banner
         displayScoresImplSystemOut.displayClosingBanner();
     }
+
+    public static Boolean isChainValid() {
+		Block currentBlock; 
+		Block previousBlock;
+		String hashTarget = new String(new char[prefix]).replace('\0', '0');
+		
+		//loop through blockchain to check hashes:
+		for(int i=1; i < newbchain.size(); i++) {
+			currentBlock = newbchain.get(i);
+			previousBlock = newbchain.get(i-1);
+			//compare registered hash and calculated hash:
+			if(!currentBlock.getHash().equals(currentBlock.calculateBlockHash()) ){
+				System.out.println("Current Hashes not equal");			
+				return false;
+			}
+			//compare previous hash and registered previous hash
+			if(!previousBlock.getHash().equals(currentBlock.getPreviousHash()) ) {
+				System.out.println("Previous Hashes not equal");
+				return false;
+			}
+			//check if hash is solved
+			if(!currentBlock.getHash().substring( 0, prefix).equals(hashTarget)) {
+				System.out.println("This block hasn't been mined "+ currentBlock.getHash() +" "+ hashTarget);
+				return false;
+			}
+		}
+		return true;
+	}
 }
