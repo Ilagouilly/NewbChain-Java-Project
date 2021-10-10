953176f81faa5db1378828370d7543b82412f956
NevilClavain <emmanuel.chaumont@wanadoo.fr>
2021-10-10 20:51:05 +0200

    cleanup

--- src/main/java/com/cryptocurrency/newbchain/Main.java
+++ src/main/java/com/cryptocurrency/newbchain/Main.java
@@ -25,21 +25,6 @@ public class Main {
 		blockChain.addBlock("Jean-françois sends 0.1n to Taqueria el farolito");
 		blockChain.addBlock("José sends 5n to Maria");
 
-		/*
-		Block block1 = new Block("Thomas sends 1n to Jean-françois", "0");
-		block1.mineBlock(prefix);
-		newbchain.add(block1);
-
-		Block block2 = new Block("Jean-françois sends 0.1n to Taqueria el farolito",
-				newbchain.get(newbchain.size() - 1).getHash());
-		block2.mineBlock(prefix);
-		newbchain.add(block2);
-
-		Block block3 = new Block("José sends 5n to Maria", newbchain.get(newbchain.size() - 1).getHash());
-		block3.mineBlock(prefix);
-		newbchain.add(block3);
-		*/
-
 		// Display all blocks
 		//displayDataImplSystemOut.displayAllBlocks(newbchain);
 
